05a62227bfe92729823cd1d4f3ea590b217caca0
brianmuvel1 <142348822+brianmuvel1@users.noreply.github.com>
2024-11-20 05:49:19 +0000

    adding adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
+import node from '@astrojs/node';
 
 import vercel from '@astrojs/vercel/serverless';
 
